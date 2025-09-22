cd62bf99fe2773f377574ef56bca9ed21b839c86
sinoh19 <60386845+sinoh19@users.noreply.github.com>
2025-09-22 21:49:20 +0900

    수정
    소스코드 수정

--- Homework_1/Homework_1.cpp
+++ Homework_1/Homework_1.cpp
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-int main()
+void check_endian()
 {
 	unsigned int value = 1;
 	unsigned char* byte_ptr = (unsigned char*)&value;
 
 	if (*byte_ptr == 1)
 	{
-		printf("Little endian\n");
+		printf("Little endian 시스템 입니다.\n");
 	}
 	else
 	{
-		printf("Big endian\n");
+		printf("Big endian 시스템 입니다.\n");
 	}
+}
 
+int main()
+{	
+	check_endian();
 	return 0;
-
 }
