0b3637f31e7e5fea34c6ae74e2d8ae60903f859a
kyu <60386845+sinoh19@users.noreply.github.com>
2025-10-08 15:17:13 +0900

    일단
    해봄 동영상 파일은 알아서

---
+++ HomeWork_3/Client/Client.cpp
@@ -0,0 +1,76 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <winsock2.h>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#pragma comment(lib, "ws2_32")
+
+#define BUFSIZE 512
+
+void err_quit(const char* msg) {
+    printf("%s\n", msg);
+    exit(1);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2 || argc > 3) {
+        printf("사용법: %s [서버IP] 파일명\n", argv[0]);
+        return 1;
+    }
+
+    const char* server_ip = "127.0.0.1";  // 기본값 로컬호스트
+    char* filename;
+
+    if (argc == 2) {
+        filename = argv[1];  // 서버 IP 입력 안 하면 기본값 사용
+    }
+    else {
+        server_ip = argv[1]; // 서버 IP 입력
+        filename = argv[2];
+    }
+
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+        return 1;
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) err_quit("소켓 생성 실패");
+
+    struct sockaddr_in serveraddr;
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(9000);
+    serveraddr.sin_addr.s_addr = inet_addr(server_ip);
+
+    if (connect(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) == SOCKET_ERROR)
+        err_quit("서버 연결 실패");
+
+    FILE* fp = fopen(filename, "rb");
+    if (!fp) {
+        printf("파일 열 수 없음: %s\n", filename);
+        closesocket(sock);
+        return 1;
+    }
+
+    // 파일 크기 전송
+    fseek(fp, 0, SEEK_END);
+    long file_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    send(sock, (char*)&file_size, sizeof(file_size), 0);
+
+    // 파일 전송
+    char buf[BUFSIZE];
+    size_t n;
+    while ((n = fread(buf, 1, BUFSIZE, fp)) > 0) {
+        send(sock, buf, n, 0);
+    }
+
+    fclose(fp);
+    closesocket(sock);
+    WSACleanup();
+
+    printf("파일 전송 완료!\n");
+    return 0;
+}
---
+++ HomeWork_3/Server/server.cpp
@@ -0,0 +1,115 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <winsock2.h>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Ws2tcpip.h>
+#pragma comment(lib, "ws2_32")
+
+#define SERVERPORT 9000
+#define BUFSIZE 512
+
+void err_quit(const char* msg)
+{
+    LPVOID IpMsgBuf;
+    FormatMessageA(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL, WSAGetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (char*)&IpMsgBuf, 0, NULL
+    );
+    MessageBoxA(NULL, (const char*)IpMsgBuf, msg, MB_ICONERROR);
+    LocalFree(IpMsgBuf);
+    exit(1);
+}
+
+void err_display(const char* msg)
+{
+    LPVOID lpMsgBuf;
+    FormatMessageA(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL, WSAGetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (char*)&lpMsgBuf, 0, NULL
+    );
+    printf("[%s] %s\n", msg, (char*)lpMsgBuf);
+    LocalFree(lpMsgBuf);
+}
+
+int main()
+{
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+        return 1;
+
+    SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sock == INVALID_SOCKET) err_quit("소켓 오류");
+    printf("서버 소켓 생성됨\n");
+
+    struct sockaddr_in serveraddr;
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(SERVERPORT);
+    if (bind(listen_sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) == SOCKET_ERROR)
+        err_quit("bind() 오류");
+
+    if (listen(listen_sock, SOMAXCONN) == SOCKET_ERROR)
+        err_quit("listen() 오류");
+
+    char buf[BUFSIZE];
+    while (1)
+    {
+        struct sockaddr_in clientaddr;
+        int addrlen = sizeof(clientaddr);
+        SOCKET client_sock = accept(listen_sock, (struct sockaddr*)&clientaddr, &addrlen);
+        if (client_sock == INVALID_SOCKET) {
+            err_display("accept()");
+            break;
+        }
+
+        char addr[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+        printf("\n클라이언트 접속: IP=%s, PORT=%d\n", addr, ntohs(clientaddr.sin_port));
+
+        // ===== 파일 수신 준비 =====
+        long file_size = 0;
+        int ret = recv(client_sock, (char*)&file_size, sizeof(file_size), 0);
+        if (ret != sizeof(file_size)) {
+            printf("파일 크기 정보 수신 실패!\n");
+            closesocket(client_sock);
+            continue;
+        }
+
+        FILE* fp = fopen("received_file.dat", "wb");
+        if (!fp) {
+            printf("파일 생성 실패!\n");
+            closesocket(client_sock);
+            continue;
+        }
+
+        // ===== 파일 데이터 수신 =====
+        long total_bytes = 0;
+        while ((ret = recv(client_sock, buf, BUFSIZE, 0)) > 0) {
+            fwrite(buf, 1, ret, fp);
+            total_bytes += ret;
+
+            // 전송률 표시
+            float percent = (float)total_bytes / file_size * 100;
+            if (percent > 100) percent = 100;
+            printf("\r수신 중: %.2f%%", percent);
+            fflush(stdout);
+        }
+        fclose(fp);
+        printf("\r수신 완료: 100.00%%\n");
+
+        closesocket(client_sock);
+        printf("클라이언트 종료: IP=%s, PORT=%d\n", addr, ntohs(clientaddr.sin_port));
+    }
+
+    closesocket(listen_sock);
+    WSACleanup();
+    return 0;
+}
